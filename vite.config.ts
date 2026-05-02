afb7d10e54d1594306832968f1fd8e861ea1b875
Franck <franck@tartatin.io>
2026-05-02 20:50:37 +0200

    Changes to deploy into GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// On GitHub Actions, GITHUB_REPOSITORY = "username/repo-name"
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
   plugins: [react()],
+  base: repoName ? `/${repoName}/` : '/',
 })
